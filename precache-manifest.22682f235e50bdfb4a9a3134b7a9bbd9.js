1baa3dd3e4e4a60f44dd3ccdaf9d9bacf9547083
kangyongseok <kangyongsuek@gmail.com>
2019-05-14 11:44:07 +0900

    Updates

---
+++ precache-manifest.22682f235e50bdfb4a9a3134b7a9bbd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5f893b26a23a11fe5b6e5009fb6397",
+    "url": "/wedding-card/index.html"
+  },
+  {
+    "revision": "92d2f5a61b9d7e68d98b",
+    "url": "/wedding-card/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/wedding-card/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "92d2f5a61b9d7e68d98b",
+    "url": "/wedding-card/static/js/main.f08e1378.chunk.js"
+  },
+  {
+    "revision": "c20a88bb2d538491efd4",
+    "url": "/wedding-card/static/js/runtime~main.16826a0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wedding-card/static/media/logo.5d5d9eef.svg"
+  }
+]);
